890b78e2254b698fb63299a7638956e75739baba
duihuhu <duihuhu@163.com>
2024-08-20 22:45:31 +0800

    debug breakdown time

--- inference/run.sh
+++ inference/run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-python cli_demo_repreated.py --prompt "A girl ridding a bike." --model_path /home/jovyan/models/CogVideoX-2b/models/snapshots/a4101b308a6a5613e240351f064d628e60324e4b/  --num_inference_steps 10 --num_videos_per_prompt 1 --height 256 --width 256
+python cli_demo_repreated.py --prompt "A girl ridding a bike." --model_path /home/jovyan/models/CogVideoX-2b/models/snapshots/a4101b308a6a5613e240351f064d628e60324e4b/  --num_inference_steps 10 --num_videos_per_prompt 1 --height 512 --width 512
 
 sleep 5
 
-python cli_demo_repreated.py --prompt "A girl ridding a bike." --model_path /home/jovyan/models/CogVideoX-2b/models/snapshots/a4101b308a6a5613e240351f064d628e60324e4b/  --num_inference_steps 20 --num_videos_per_prompt 1 --height 256 --width 256
+python cli_demo_repreated.py --prompt "A girl ridding a bike." --model_path /home/jovyan/models/CogVideoX-2b/models/snapshots/a4101b308a6a5613e240351f064d628e60324e4b/  --num_inference_steps 20 --num_videos_per_prompt 1 --height 512 --width 512
 
 sleep 5
 
-python cli_demo_repreated.py --prompt "A girl ridding a bike." --model_path /home/jovyan/models/CogVideoX-2b/models/snapshots/a4101b308a6a5613e240351f064d628e60324e4b/  --num_inference_steps 30 --num_videos_per_prompt 1 --height 256 --width 256
+python cli_demo_repreated.py --prompt "A girl ridding a bike." --model_path /home/jovyan/models/CogVideoX-2b/models/snapshots/a4101b308a6a5613e240351f064d628e60324e4b/  --num_inference_steps 30 --num_videos_per_prompt 1 --height 512 --width 512
 
 sleep 5
 
-python cli_demo_repreated.py --prompt "A girl ridding a bike." --model_path /home/jovyan/models/CogVideoX-2b/models/snapshots/a4101b308a6a5613e240351f064d628e60324e4b/  --num_inference_steps 40 --num_videos_per_prompt 1 --height 256 --width 256
+python cli_demo_repreated.py --prompt "A girl ridding a bike." --model_path /home/jovyan/models/CogVideoX-2b/models/snapshots/a4101b308a6a5613e240351f064d628e60324e4b/  --num_inference_steps 40 --num_videos_per_prompt 1 --height 512 --width 512
 sleep 5
 
-python cli_demo_repreated.py --prompt "A girl ridding a bike." --model_path /home/jovyan/models/CogVideoX-2b/models/snapshots/a4101b308a6a5613e240351f064d628e60324e4b/  --num_inference_steps 50 --num_videos_per_prompt 1 --height 256 --width 256
+python cli_demo_repreated.py --prompt "A girl ridding a bike." --model_path /home/jovyan/models/CogVideoX-2b/models/snapshots/a4101b308a6a5613e240351f064d628e60324e4b/  --num_inference_steps 50 --num_videos_per_prompt 1 --height 512 --width 512
